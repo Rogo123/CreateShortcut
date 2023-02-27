da6acd52a8f6588861a0283dd3fed4a0672e8487
Roger <roger.oshaug@ror-ikt.no>
2023-02-27 14:59:46 +0100

    Initial

---
+++ CreateShortcut.ps1
@@ -0,0 +1,7 @@
+﻿# Create a Shortcut with Windows PowerShell
+$TargetFile = "\\S1547-1TID-001.login.romsdalsnett.no\WinTid$\Dashboard\Dashboard.exe"
+$ShortcutFile = "c:\users\32946\Desktop\WinTid.lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.TargetPath = $TargetFile
+$Shortcut.Save()
